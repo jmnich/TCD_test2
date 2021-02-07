835876f60c8dcb0de9722b119791df16409d1f3f
Jakub Mnich <jk.mbot@gmail.com>
2021-02-07 18:05:10 +0100

    basic acquisition works

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -231,7 +231,6 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 }
 
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
-	HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
 	tcdDrv->ICGPulseCallback();
 }
 
